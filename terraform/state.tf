588a3d79afcac87c2cd48bf29f2ecb0919f5e42d
JDTweddle <149077184+JDTweddle@users.noreply.github.com>
2025-02-02 23:02:12 +0000

    Update state.tf

--- terraform/state.tf
+++ terraform/state.tf
@@ -1,8 +1,8 @@
 terraform {
     backend "s3" {
-        bucket = "patchez-terraform-state"
+        bucket = "your-bucket-name"
         key = "global/s3/terrafrom.tfstate"
-        region = "eu-west-2"
+        region = "your-region"
         dynamodb_table = "s3-tf-state-table"
         
     }
